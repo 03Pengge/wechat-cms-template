f1db49e24726aae2d271cbaca61788586607df3d
代云舟 <daiyunz@chanjet.com>
2021-11-24 23:07:15 +0800

    feat: 添加拖拽功能

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -16,8 +16,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(css|less)$/i,
+        use: ['style-loader', 'css-loader','less-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
---
+++ src/entry/index.js
@@ -0,0 +1,22 @@
+import './index.less';
+
+const dom = document.querySelector;
+console.log(dom);
+document.querySelector('.comp-draggable').ondragstart = function(e){
+    e.dataTransfer.setData("Text",e.target.id);
+    console.log(e);
+}
+document.querySelector('.dai-preview-content').ondragover = (e)=>{
+    e.preventDefault();
+}
+ 
+document.querySelector('.dai-preview-content').ondrop = function(e)
+{
+    e.preventDefault();
+    var id=e.dataTransfer.getData("Text");
+    const dragDom = document.getElementById(id);
+    const node = dragDom.cloneNode(true);
+    const timeStamp = new Date().getTime();
+    node.setAttribute('id',timeStamp);
+    document.querySelector('.dai-preview-content').appendChild(node);
+}
