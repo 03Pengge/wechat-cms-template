a7c220ddf76b40aab3eafce81c3d17a656e05535
daiyunzhou <daiyunz@chanjet.com>
2021-11-25 12:42:22 +0800

    feat: html加载本地资源

--- src/entry/index.js
+++ src/entry/index.js
@@ -1,22 +1,24 @@
-import './index.less';
+import "./index.less";
 
-const dom = document.querySelector;
-console.log(dom);
-document.querySelector('.comp-draggable').ondragstart = function(e){
-    e.dataTransfer.setData("Text",e.target.id);
+window.onload = () => {
+  const dom = document.querySelector;
+  console.log(dom);
+  document.querySelector(".comp-draggable").ondragstart = function (e) {
+    e.dataTransfer.setData("Text", e.target.id);
     console.log(e);
-}
-document.querySelector('.dai-preview-content').ondragover = (e)=>{
+  };
+  document.querySelector(".dai-preview-content").ondragover = (e) => {
     e.preventDefault();
-}
- 
-document.querySelector('.dai-preview-content').ondrop = function(e)
-{
+  };
+
+  document.querySelector(".dai-preview-content").ondrop = function (e) {
     e.preventDefault();
-    var id=e.dataTransfer.getData("Text");
+    var id = e.dataTransfer.getData("Text");
     const dragDom = document.getElementById(id);
     const node = dragDom.cloneNode(true);
     const timeStamp = new Date().getTime();
-    node.setAttribute('id',timeStamp);
-    document.querySelector('.dai-preview-content').appendChild(node);
-}
+    node.setAttribute("id", timeStamp);
+    document.querySelector(".dai-preview-content").appendChild(node);
+  };
+};
+// https://blog.csdn.net/weixin_37722222/article/details/81530629
